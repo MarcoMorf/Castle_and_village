324c89b02ce45d18f1fa57f8a4d1b49a6f048032
Oliver Dressler <odressler@gmail.com>
2021-02-09 17:23:14 +0100

    New health system and some refactoring of config
    Description of health system in comments (config, at top)

--- main.js
+++ main.js
@@ -1,75 +1,121 @@
 
-let vampire_gamestage = {"action_left":15,
-                        "num_actions": 15,
-                        "current_position_vampire":0,
-                        "blood_absorbed_today":0,
-                        "blood_absorbed_alltime":0,
-                        "strawberry_count":0,
-                        "blood_needed_for_day":4,
-                        "left_castle":false};
-let villager_gamestage ={"villager":{"human1":{"blood":{"current":5,
-                                                        "max":5
-                                                     },
-                                            "position":1
-                                            },
-                                   "human2":{"blood":{"current":3,
-                                                        "max":3
-                                                     },
-                                            "position":2
-                                            },
-                                   "human3":{"blood":{"current":2,
-                                                        "max":2
-                                                     },
-                                            "position":3
-                                            },
-                                   "human4":{"blood":{"current":6,
-                                                        "max":6
-                                                     },
-                                            "position":4
-                                            }
 
-                                    }
+// Config doesnt change over time,
+// gamestages do
+let config = {
+    "vampire": {
+        "num_actions": 15,
+        "blood_needed_for_day": 4,
+    },
 
-                        };
+    // New health system (probability-based)
+    // Each villager has 3 possible states: dead <- sick <-> healthy
+    // healthy -> sick: 
+    //      based on amount of blood lost (e.g. mam 3 / 5 blood: blood_lost = 2)
+    //      lost_blood is used to lookup the probability that this villager gets sick
+    //
+    // sick -> healthy:
+    //      Fixed probability to heal. Simple for now.
+    //      Strawberry heals?
+    
+    "get_sick": [0, 0.3, 0.5, 0.8, 0.8, 1.0, 1.0, 1.0, 1.0], // e.g. A villager with 2 blood_lost has a probability of 50% to get sick
+    "get_healthy": 0.5,
 
+    // Blood healed per round:
+    // e.g.If villager is healthy: [1, 2]   We randomly pick one from this list
+    "heal_healthy": [1, 2],
+    "heal_sick": [0, 1],
+    
+    "villager": {
+        1: {"blood_max": 5},
+        2: {"blood_max": 3},
+        3: {"blood_max": 2},
+        4: {"blood_max": 6},
+    }
+};
+
+let vampire_gamestage = {
+    "action_left": 15,
+    "current_position_vampire": 0,
+    "blood_absorbed_today": 0,
+    "blood_absorbed_alltime": 0,
+    "strawberry_count": 0,
+    "left_castle": false
+};
+
+let villager_gamestage = {
+    1: {"blood": 5, "state": "healthy"},    
+    2: {"blood": 3, "state": "healthy"},
+    3: {"blood": 2, "state": "healthy"},
+    4: {"blood": 6, "state": "healthy"}
+};
+
+
+function random_choice(array){
+    return array[Math.floor(Math.random() * array.length)]
+}
 
 function update_villager_health_end_of_round(){
     for (let ind = 1; ind < 5; ind++) {
-        var state = get_villager_state(ind);
-        
+        let villager = villager_gamestage[ind];
+
+        state = get_villager_state(ind);
+
+        // First we check if this villager transistions to another health state
+        if (state == "healthy"){
+            // Villager could get sick (certain probability)
+            let blood_lost = config["villager"][ind]["blood_max"] - villager["blood"];
+            let prob = config["get_sick"][blood_lost];
+            if (Math.random() < prob){
+                villager_gamestage[ind]["state"] = "sick"; // Update the state (is sick now)
+            }
+        } else if (state == "sick"){
+            // Villager could get healthy
+            if (Math.random() < config["get_healthy"]){
+                villager_gamestage[ind]["state"] = "healthy";
+            }
+        }
+
+        // Then we update the villager based on its state
+        // Randomly choosing how much to heal
+        state = get_villager_state(ind);
         if(state == "healthy"){
-            villager_gamestage["villager"]["human"+ind]["blood"]["current"] += 1;
+            villager_gamestage[ind]["blood"] += random_choice(config["heal_healthy"]);
         }
         if (state == "sick"){
-            villager_gamestage["villager"]["human"+ind]["blood"]["current"] += 0;
+            villager_gamestage[ind]["blood"] += random_choice(config["heal_sick"]);
         }
         if(state == "dead"){
-            villager_gamestage["villager"]["human"+ind]["blood"]["current"]=0;
+            villager_gamestage[ind]["blood"] = 0;
         }
-        max=villager_gamestage["villager"]["human"+ind]["blood"]["max"]
-        if(villager_gamestage["villager"]["human"+ind]["blood"]["current"]>max){
-            villager_gamestage["villager"]["human"+ind]["blood"]["current"]=max;
 
+        max = config["villager"][ind]["blood_max"];
+
+        if(villager_gamestage[ind]["blood"] > max){
+            villager_gamestage[ind]["blood"] = max;
         }
     }
         
 }
 
 function get_villager_state(ind){
-    var vblood = villager_gamestage["villager"]["human"+ind]["blood"]
-    blood_content = vblood["current"];
-    blood_content_upper = vblood["max"];
-    blood_content_threshold = blood_content_upper - 1;
+    if (villager_gamestage[ind]["blood"] == 0){
+        villager_gamestage[ind]["state"] = "dead";
+    }
+    return villager_gamestage[ind]["state"]
+    // blood_content = villager_gamestage[ind]["blood"];
+    // blood_content_upper = config["villager"][ind]["blood_max"];
+    // blood_content_threshold = blood_content_upper - 1;
     
-    if(blood_content_threshold<=blood_content && blood_content <= blood_content_upper){
-        return "healthy";
-    }
-    if(1<=blood_content&&blood_content<blood_content_threshold){
-        return "sick";
-    }
-    if(blood_content<=0){
-        return "dead";
-    }
+    // if(blood_content_threshold<=blood_content && blood_content <= blood_content_upper){
+    //     return "healthy";
+    // }
+    // if(1<=blood_content&&blood_content<blood_content_threshold){
+    //     return "sick";
+    // }
+    // if(blood_content<=0){
+    //     return "dead";
+    // }
 }
 
 function game_over(){
@@ -84,7 +130,7 @@ function game_over(){
     // Round is over. Do some accounting
     if (do_it) {
         // Blood every round
-        vampire_gamestage["blood_absorbed_alltime"] -= vampire_gamestage["blood_needed_for_day"];
+        vampire_gamestage["blood_absorbed_alltime"] -= config["vampire"]["blood_needed_for_day"];
 
         // blood to go home
         extra_blood = vampire_gamestage["current_position_vampire"] - vampire_gamestage["action_left"];
@@ -99,7 +145,7 @@ function game_over(){
             location.reload()
         }
         vampire_gamestage["current_position_vampire"]=0;
-        vampire_gamestage["action_left"] = vampire_gamestage["num_actions"];
+        vampire_gamestage["action_left"] = config["vampire"]["num_actions"];
         vampire_gamestage["left_castle"]=false;
         update_villager_health_end_of_round();
     }
@@ -109,10 +155,10 @@ function game_over(){
 
 function bite(){
     pos = vampire_gamestage["current_position_vampire"];
-    if(villager_gamestage["villager"]["human"+pos]["blood"]["current"]>0){
-        villager_gamestage["villager"]["human"+pos]["blood"]["current"]-=1;
-        vampire_gamestage["blood_absorbed_alltime"]+=1;
-        vampire_gamestage["blood_absorbed_today"]+=1;
+    if(villager_gamestage[pos]["blood"] > 0){
+        villager_gamestage[pos]["blood"] -= 1;
+        vampire_gamestage["blood_absorbed_alltime"] += 1;
+        vampire_gamestage["blood_absorbed_today"] += 1;
         vampire_gamestage["action_left"] -= 2;
         game_over()
     }
@@ -141,8 +187,8 @@ function get_strawberry(){
 
 function give_strawberry(){
     pos = vampire_gamestage["current_position_vampire"];
-    if(villager_gamestage["villager"]["human"+pos]["blood"]["current"]>0 && vampire_gamestage["strawberry_count"]>0){
-        villager_gamestage["villager"]["human"+pos]["blood"]["current"]+=3;
+    if(villager_gamestage[pos]["blood"] > 0 && vampire_gamestage["strawberry_count"]>0){
+        villager_gamestage[pos]["blood"] += 3;
         // vampire_gamestage["action_left"]-=1;
         vampire_gamestage["strawberry_count"]-=1
         game_over()
@@ -167,10 +213,8 @@ function update_all(){
 
     // Update all villagers
     for (let ind = 1; ind < 5; ind++) {
-        villager = villager_gamestage["villager"]["human"+ind];
-
         type = "";
-        switch(villager["position"]){
+        switch(ind){
             case 1: 
                 type = "mam";
                 break;
@@ -184,6 +228,7 @@ function update_all(){
                 type = "dad";
                 break;
         }
+
         state = get_villager_state(ind);
         if (state == "healthy") {
             state = "";
@@ -206,8 +251,8 @@ function update_all(){
 
     // update health of villager
     for (let ind = 1; ind < 5; ind++) {
-        $("#health_pos"+ind).text(villager_gamestage["villager"]["human"+ind]["blood"]["current"]);
-        $("#max_health_pos"+ind).text(villager_gamestage["villager"]["human"+ind]["blood"]["max"]);
+        $("#health_pos"+ind).text(villager_gamestage[ind]["blood"]);
+        $("#max_health_pos"+ind).text(config["villager"][ind]["blood_max"]);
     }
 
     //update strawberrycount and blood of vampire
@@ -215,9 +260,7 @@ function update_all(){
     $("#strawberry_count").text(vampire_gamestage["strawberry_count"]);
 
     // Update blood per round
-    $("#blood_per_round").text(vampire_gamestage["blood_needed_for_day"]);
-
-
+    $("#blood_per_round").text(config["vampire"]["blood_needed_for_day"]);
 }
 
 
